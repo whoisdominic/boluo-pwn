ceef6074fc3e63cd51cbaeb349cb6d228bf346c0
Dominic <dominicshajaa@icloud.com>
2022-07-14 19:33:08 -0700

    Tauri & tailwind setup done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# boluo-pwn
+# 菠萝黑客 - Bōluó Pwn
+
+带有内置黑客脚本的桌面应用程序
+
 A desktop GUI for the Pineapple Wifi - Built in pwning scripts
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="flex justify-center self-center items-center w-screen h-screen flex-col">
+        <h2 className="text-white-400 text-3xl m-20">欢迎来到</h2>
+        <h1 className="text-yellow-400 text-9xl animate-bounce">菠萝</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
